b6f838b05b9500587eea665557ca3ad88f8be6f1
Ice Nantawat <nuntawat_168@outlook.com>
2023-09-22 22:06:28 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-[#171823]  w-screen h-screen flex flex-col justify-start items-center">
+      <div className="mt-[70px]"></div>
+      <p className="text-white text-[40px] font-normal tracking-[15px]">TODO</p>
+    </div>
+  );
+}
+
+export default App;
