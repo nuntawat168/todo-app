28eaaf3c20cfd10e73a75f53bd11bc0493057751
Ice Nantawat <nuntawat_168@outlook.com>
2023-10-05 02:36:36 +0700

    fix: input field can't typing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AppContainers from "./components/AppContainers";
 import AppHeader from "./components/AppHeader";
 function App() {
   const [inputTodo, setInputTodo] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+  const [lastTodoItemId, setLastTodoItemId] = useState(0);
+
+  function handleOnChangeInputTodo(event) {
+    setInputTodo(event.target.value);
+  }
+
+  function handleOnKeyDownInputTodo(event) {
+    if (event.key !== "Enter") return;
+    if (!event.target.value.trim()) return;
+    const newTodoItem = {
+      id: lastTodoItemId + 1,
+      todo: event.target.value,
+      status: "active",
+    };
+    setLastTodoItemId((prev) => prev + 1);
+    setTodoItems([...todoItems, newTodoItem]);
+    setInputTodo("");
+  }
+
+  useEffect(() => {
+    console.log("todoItems:", todoItems);
+  }, [todoItems]);
 
   return (
     <AppContainers>
       <AppHeader />
-      <div className="w-full h-12 bg-white rounded-[5px] pl-5 py-[14px] flex space-x-3">
+      <div className="w-full h-auto bg-white rounded-[5px] pl-5 py-[14px] flex flex-row justify-center items-center space-x-3">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="20"
@@ -28,7 +51,7 @@ function App() {
             opacity="0.01"
             d="M6.66675 10.2534L8.91333 12.5L13.9133 7.5"
             stroke="white"
-            stroke-width="2"
+            strokeWidth="2"
           />
           <defs>
             <linearGradient
@@ -39,12 +62,20 @@ function App() {
               y2="30"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#55DDFF" />
-              <stop offset="1" stop-color="#C058F3" />
+              <stop stopColor="#55DDFF" />
+              <stop offset="1" stopColor="#C058F3" />
             </linearGradient>
           </defs>
         </svg>
-        <input type="text" placeholder="Create a new todo" className="mt-1" />
+        <input
+          value={inputTodo}
+          aria-label="inputTodo"
+          type="text"
+          placeholder="Create a new todo"
+          onChange={handleOnChangeInputTodo}
+          onKeyDown={handleOnKeyDownInputTodo}
+          className="w-full h-full grow mt-1 focus:outline-none text-[#393A4B] placeholder:text-[#9495A5] "
+        />
       </div>
     </AppContainers>
   );
