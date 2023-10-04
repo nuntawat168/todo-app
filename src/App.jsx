2e9462fa6714575f7b8ab5f92c35385c782e5b05
Ice Nantawat <nuntawat_168@outlook.com>
2023-10-05 02:50:05 +0700

    feat: add todo context

--- src/App.jsx
+++ src/App.jsx
@@ -2,82 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import AppContainers from "./components/AppContainers";
 import AppHeader from "./components/AppHeader";
+import InputTodoField from "./components/InputTodoField";
+import { TodoProvider } from "./contexts/todoContext";
 function App() {
-  const [inputTodo, setInputTodo] = useState("");
-  const [todoItems, setTodoItems] = useState([]);
-  const [lastTodoItemId, setLastTodoItemId] = useState(0);
-
-  function handleOnChangeInputTodo(event) {
-    setInputTodo(event.target.value);
-  }
-
-  function handleOnKeyDownInputTodo(event) {
-    if (event.key !== "Enter") return;
-    if (!event.target.value.trim()) return;
-    const newTodoItem = {
-      id: lastTodoItemId + 1,
-      todo: event.target.value,
-      status: "active",
-    };
-    setLastTodoItemId((prev) => prev + 1);
-    setTodoItems([...todoItems, newTodoItem]);
-    setInputTodo("");
-  }
-
-  useEffect(() => {
-    console.log("todoItems:", todoItems);
-  }, [todoItems]);
-
   return (
-    <AppContainers>
-      <AppHeader />
-      <div className="w-full h-auto bg-white rounded-[5px] pl-5 py-[14px] flex flex-row justify-center items-center space-x-3">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-        >
-          <circle cx="10" cy="10" r="9.5" fill="white" stroke="#E3E4F1" />
-          <circle
-            opacity="0.01"
-            cx="10"
-            cy="10"
-            r="10"
-            fill="url(#paint0_linear_0_343)"
-          />
-          <path
-            opacity="0.01"
-            d="M6.66675 10.2534L8.91333 12.5L13.9133 7.5"
-            stroke="white"
-            strokeWidth="2"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_0_343"
-              x1="-10"
-              y1="10"
-              x2="10"
-              y2="30"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#55DDFF" />
-              <stop offset="1" stopColor="#C058F3" />
-            </linearGradient>
-          </defs>
-        </svg>
-        <input
-          value={inputTodo}
-          aria-label="inputTodo"
-          type="text"
-          placeholder="Create a new todo"
-          onChange={handleOnChangeInputTodo}
-          onKeyDown={handleOnKeyDownInputTodo}
-          className="w-full h-full grow mt-1 focus:outline-none text-[#393A4B] placeholder:text-[#9495A5] "
-        />
-      </div>
-    </AppContainers>
+    <TodoProvider>
+      <AppContainers>
+        <AppHeader />
+        <InputTodoField />
+      </AppContainers>
+    </TodoProvider>
   );
 }
 
