bdfce12882dff4c0d7f7cd534552e9abfcb68726
Ice Nantawat <nuntawat_168@outlook.com>
2023-10-05 02:50:05 +0700

    feat: add todo context

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,6 @@ import TodoItems from "./components/TodoItems";
 import AppFooter from "./components/AppFooter";
 import SelectFilterTodoItems from "./components/SelectFilterTodoItems";
 function App() {
-  const [inputTodo, setInputTodo] = useState("");
-  const [todoItems, setTodoItems] = useState([]);
-  const [lastTodoItemId, setLastTodoItemId] = useState(0);
-
-  function handleOnChangeInputTodo(event) {
-    setInputTodo(event.target.value);
-  }
-
-  function handleOnKeyDownInputTodo(event) {
-    if (event.key !== "Enter") return;
-    if (!event.target.value.trim()) return;
-    const newTodoItem = {
-      id: lastTodoItemId + 1,
-      todo: event.target.value,
-      status: "active",
-    };
-    setLastTodoItemId((prev) => prev + 1);
-    setTodoItems([...todoItems, newTodoItem]);
-    setInputTodo("");
-  }
-
-  useEffect(() => {
-    console.log("todoItems:", todoItems);
-  }, [todoItems]);
-
   return (
     <TodoProvider>
       <AppContainers>
